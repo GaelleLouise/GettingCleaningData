3cba5da2d47733d00286c3a44114a974be98b9c3
GaelleLouise <gaelle.louise.us@gmail.com>
2020-12-08 11:33:56 +0100

    2 ajouts : 1. fichier Excel (KO car impossible d'installer le package) 2. fichier XML (à rapatrier en local avant d'essayer de le lire)

--- quiz_W1.R
+++ quiz_W1.R
@@ -3,6 +3,7 @@ if(!file.exists("data")) {
      dir.create("data")
 }
 
+## fichier CSV :
 # téléchargement du fichier :
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileUrl, destfile = "./data/housing_Idaho.csv", method = "curl")
@@ -15,3 +16,30 @@ dateDownload
 # intégration des données dans un dataframe data :
 data <- read.csv("./data/housing_Idaho.csv")
 
+
+## fichier Excel :
+# téléchargement du fichier :
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl, destfile = "./data/GasAcquisProgram.xlsx", method = "curl")
+dateDownload <- date()
+
+dat <- read.xlsx("./data/GasAcquisProgram.xlsx", colIndex = c(7:15), rowIndex = c(18:23), header = TRUE)
+
+# read.xlsx ne fonctionne pas => toutes les installs de packages liés à Excel sont KO à cause de Java
+# => passage par un fichier txt :
+dat <- read.table("./data/GasAcquisProgram.txt", sep = "\t", header = TRUE)
+
+
+## fichier XML :
+# téléchargement du fichier :
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- xmlTreeParse(fileURL, useInternal = TRUE) # renvoie une erreur car ne semble pas gérer le httpS
+# => tentative de chargement du fichier en local :
+fileURL <- "./data/getdata_data_restaurants.XML"
+doc <- xmlTreeParse(fileURL, useInternal = TRUE)
+# cette fois, c'est bon !
+rootNode <- xmlRoot(doc)
+xmlName(rootNode) # => response
+names(rootNode) # => row
+zipcodes <- xpathSApply(rootNode,"//zipcode", xmlValue) # stocke l'ensemble des zipcodes dans une variable
+sum(zipcodes == "21231") # donne le nb de zipcodes égaux à 21231
