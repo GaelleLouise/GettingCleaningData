a88e8286d5756f5f39847f99cf736658445fa825
GaelleLouise <gaelle.louise.us@gmail.com>
2020-12-09 19:50:46 +0100

    sauvegarde après question 2

--- quiz_W3.R
+++ quiz_W3.R
@@ -1,11 +1,10 @@
+## Question 1
 # téléchargement du fichier :
 if(!file.exists("./data")) {dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileUrl, destfile = "./data/housing_Idaho.csv", method = "curl")
-
 # date du téléchargement :
 dateDownload <- date()
-
 # intégration des données dans un dataframe data :
 data <- read.csv("./data/housing_Idaho.csv")
 
@@ -13,4 +12,16 @@ data <- read.csv("./data/housing_Idaho.csv")
 agricultureLogical <- data[,"ACR"] == 3 & data[,"AGS"] == 6
 data[which(agricultureLogical),]
 
+
+## Question 2
+# téléchargement du fichier :
+if(!file.exists("./data")) {dir.create("./data")}
+filejpeg <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+download.file(filejpeg, destfile = "./data/photo.jpg", method = "curl")
+dateDownload <- date()
+# chargement du package jpeg :
+library(jpeg)
+# stockage de la photo et application du quantile voulu (0.3 -> 0.8)
+photo <- readJPEG("./data/photo.jpg", native = TRUE)
+quantile(photo, probs = c(0.3, 0.8))
 
