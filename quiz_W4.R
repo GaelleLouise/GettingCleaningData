f70f89a02d5b52cda8451d730be177e54b05c74e
GaelleLouise <gaelle.louise.us@gmail.com>
2020-12-10 15:44:14 +0100

    Update avec questions 3 & 4 incluses

--- quiz_W4.R
+++ quiz_W4.R
@@ -28,3 +28,22 @@ gross <- read.csv("./data/Gross_Domestic_Product.csv", skip = 5, header = FALSE,
                                 ,"Vide2","Vide3","Vide4","Vide5","Vide6"), nrows = 190)
 # suppression des virgules du champ GDP puis conversion en numérique pour pouvoir faire la moyenne :
 mean(as.numeric(gsub(",","",gross$GrossDomesticProduct)), na.rm = TRUE)
+
+
+## Question 3
+grep("^United", gross$CountryName)
+# => donne un vecteur de 3 éléments => 3 pays ont le nom qu icommence par "United"
+
+
+## Questin 4
+# file Educational data:
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(fileUrl, destfile = "./data/Education.csv", method = "curl")
+# date du téléchargement :
+dateDownload <- date()
+# intégration des données dans un dataframe data :
+educ <- read.csv("./data/Education.csv")
+GrossEduc = merge(gross, educ, all = FALSE)
+
+# l'année fiscale est cachée dans le champ "Special.Notes"
+grep("iscal year end: June 30", GrossEduc$Special.Notes, value = TRUE)
